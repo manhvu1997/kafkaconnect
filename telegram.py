3c1f9f74daa84d2e54123a053351f51bb168db60
manhvu1997 <58839875+manhvu1997@users.noreply.github.com>
2020-10-28 08:41:06 +0700

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -146,3 +146,113 @@ class Ticket():
         except Exception as e:
             logging.error("Ticket ticket_status: %s"%(e))
             raise e
+class Database():
+
+    def __init__(self):
+        # load database config from config file
+        try:
+            dir_path = path.dirname(path.abspath(__file__))
+            filename = path.join(dir_path, "config.json")
+            with open(filename, "r") as config_file:
+                self.config = json.load(config_file)
+                self.host = self.config["database"]["host"]
+                self.dbname = self.config["database"]["dbname"]
+                self.table = self.config["database"]["table"]
+                self.usr = self.config["database"]["user"]
+                self.passwd = self.config["database"]["pass"]
+        except Exception as e:
+            raise e
+        # create a connection to database
+        try:
+            self.cnx = mysql.connector.connect(user=self.usr, password=self.passwd, host=self.host,
+                                               database=self.dbname)
+            self.cursor = self.cnx.cursor(buffered=True)
+        except Exception as e:
+            raise e
+
+    def get_data(self):
+        # get ticket info from database
+        try:
+            query = self.config["database"]["query"]["get_data"]
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            return data
+        except Exception as e:
+            raise e
+
+    def insert(self, *values):
+        # insert new data to database
+        try:
+            query = self.config["database"]["query"]["insert"]
+            self.cursor.execute(query, values)
+            # Commit your changes in the database
+            self.cnx.commit()
+            status = 1
+        except Exception as e:
+            status = -1
+            logging.error("Database insert: %s"%(e))
+            raise e
+        finally:
+            return status
+
+    def update(self, ticket_code, ticket_status):
+        # update data in database when is changed
+        try:
+            query = self.config["database"]["query"]["update"]
+            self.cursor.execute(query, (ticket_status, ticket_code,))
+            self.cnx.commit()
+        except Exception as e:
+            logging.error("Database update: %s"%(e))
+            raise e
+
+    def delete(self, ticket_code):
+        # delete data in database
+        try:
+            query = self.config["database"]["query"]["delete"]
+            self.cursor.execute(query, (ticket_code,))
+            self.cnx.commit()
+        except Exception as e:
+            logging.error("Database delete: %s"%(e))
+            raise e
+
+    def is_exist(self, ticket_code):
+        # search Ticket code in database, return 1 if exist, else return 0
+        try:
+            query = self.config["database"]["query"]["is_exist"]
+            self.cursor.execute(query, (ticket_code,))
+            data = self.cursor.fetchall()
+            if data:
+                # ticket is exist in db
+                status = 1
+            else:
+                # ticket not is exist in db
+                status = 0
+        except Exception as e:
+            status = -1
+            raise e
+        finally:
+            return status
+
+    def get_status(self, ticket_code):
+        # get ticket status from database
+        try:
+            query = self.config["database"]["query"]["ticket_status"]
+            self.cursor.execute(query, (ticket_code,))
+            data = self.cursor.fetchall()
+            return data
+        except Exception as e:
+            raise e
+
+    def get_long_time_data(self, timeline):
+        # get ticket data from database
+        try:
+            query = self.config["database"]["query"]["long_time_data"]
+            self.cursor.execute(query, (timeline,))
+            data = self.cursor.fetchall()
+            return data
+        except Exception as e:
+            raise e
+
+    def __del__(self):
+        self.cnx.close()
+        self.cursor.close()            
